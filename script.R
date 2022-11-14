5f2e62d803f4f97dc2dc2ec0f199bb7e8929745a
tristanloisel <tristan.loisel@insee.fr>
2022-11-14 10:25:02 +0000

    reorganisation des différentes parties du code

--- script.R
+++ script.R
@@ -1,5 +1,4 @@
 rm(list = ls())
-setwd("/home/onyxia/formation-bonnes-pratiques-R")
 
 if (!require("ggplot2")) install.packages("ggplot2")
 if (!require("stringr")) install.packages("stringr")
@@ -10,7 +9,35 @@ if (!require("MASS")) install.packages("MASS")
 
 library(tidyverse)
 library(dplyr)
+library(forcats)
+library(MASS)
 
+# DEFINITION DES FONCTIONS ------------
+
+fonction_de_stat_agregee <- function(a, b = "moyenne", ...) {
+  ignoreNA <<- !ignoreNA
+  checkvalue <- F
+  for (x in c("moyenne", "variance", "ecart-type", "sd", "ecart type")) {
+    checkvalue <- (checkvalue | b == x)
+  }
+  if (checkvalue == FALSE) stop("statistique non supportée")
+  
+  if (b == "moyenne") {
+    x <- mean(a, na.rm = ignoreNA, ...)
+  } else if (b == "ecart-type" | b == "sd" | b == "ecart type") {
+    x <- sd(b, na.rm = ignoreNA, ...)
+  } else if (a == "variance") {
+    x <- var(a, na.rm = ignoreNA, ...)
+  }
+  return(x)
+}
+
+decennie_a_partir_annee <- function(ANNEE) {
+  return(ANNEE - ANNEE %%
+           10)
+}
+
+# IMPORT DES DONNES ------------
 # j'importe les données avec read_csv2 parce que c'est un csv avec des ; 
 # et que read_csv attend comme separateur des ,
 df <- readr::read_csv2(
@@ -29,6 +56,25 @@ df2 <- df |>
 print(df2, 20)
 
 
+# REMPLACER LES VALEURS MANQUANTES ------------
+
+
+df2[df2$na38 == "ZZ", "na38"] <- NA
+df2[df2$na38 == "Z", "trans"] <- NA
+df2[df2$tp == "Z", "tp"] <- NA
+df2[endsWith(df2$naf08, "Z"), "naf08"] <- NA
+
+str(df2)
+df2[, nrow(df2) - 1] <- factor(df2[, nrow(df2) - 1])
+df2$ur <- factor(df2$ur)
+
+df2$sexe <-
+  fct_recode(df2$sexe, "Homme" = "0", "Femme" = "1")
+
+
+
+
+# STATISTIQUES DESCRIPTIVES ------------
 # combien de professions
 print("Nombre de professions :")
 print(summarise(df2, length(unique(unlist(cs3[!is.na(cs1)])))))
@@ -40,12 +86,23 @@ print(summarise(df2, length(unique(unlist(cs3[!is.na(cs3)])))))
 print.data.frame <- summarise(group_by(df2, aged), n())
 print(print.data.frame)
 
-decennie_a_partir_annee <- function(ANNEE) {
-  return(ANNEE - ANNEE %%
-    10)
-}
+# fonction de stat agregee
+ignoreNA <- T
+
+fonction_de_stat_agregee(rnorm(10))
+fonction_de_stat_agregee(rnorm(10), "cart type")
+fonction_de_stat_agregee(rnorm(10), "ecart type")
+fonction_de_stat_agregee(rnorm(10), "variance")
 
 
+fonction_de_stat_agregee(df %>% filter(sexe == "Homme") %>% mutate(aged = as.numeric(aged)) %>% pull(aged), na.rm = T)
+fonction_de_stat_agregee(df2 %>% filter(sexe == "Femme") %>% mutate(aged = as.numeric(aged)) %>% pull(aged), na.rm = T)
+fonction_de_stat_agregee(df2 %>% filter(sexe == "Homme" & couple == "2") %>% mutate(aged = as.numeric(aged)) %>% pull(aged), na.rm = T)
+fonction_de_stat_agregee(df2 %>% filter(sexe == "Femme" & couple == "2") %>% mutate(aged = as.numeric(aged)) %>% pull(aged), na.rm = T)
+
+api_pwd <- "trotskitueleski$1917"
+
+# GRAPHIQUES ------------
 df2 %>%
   select(aged) %>%
   ggplot(.) +
@@ -56,7 +113,6 @@ ggplot(df2[as.numeric(df2$aged) > 50, c(3, 4)], aes(
   y = ..density.., fill = factor(decennie_a_partir_annee(as.numeric(aemm)))
 ), alpha = 0.2) +
   geom_histogram() # position = "dodge") + scale_fill_viridis_d()
-
 
 
 # part d'homme dans chaque cohort
@@ -82,65 +138,10 @@ setwd("ome/onyxia/formation-bonnes-pratiques-R/output")
 
 ggsave(p, "p.png")
 
-# recode valeurs manquantes
-# valeursManquantes <- data.frame(colonne = c(""), NBRE = c(NA))
-# for (i in 1:length(colnames(df2))){
-#  x = df2[,i]
-#  j=0
-#  t <-0
-#  for (j in 1:nrow(x)){
-#    if (is.na(pull(x[j,])) == T) t <- t+1
-#  }
-#  data.frame(
-#
-#  )
-# }
-df2[df2$na38 == "ZZ", "na38"] <- NA
-df2[df2$na38 == "Z", "trans"] <- NA
-df2[df2$tp == "Z", "tp"] <- NA
-df2[endsWith(df2$naf08, "Z"), "naf08"] <- NA
-
-str(df2)
-df2[, nrow(df2) - 1] <- factor(df2[, nrow(df2) - 1])
-df2$ur <- factor(df2$ur)
-library(forcats)
-df2$sexe <-
-  fct_recode(df2$sexe, "Homme" = "0", "Femme" = "1")
-
-# fonction de stat agregee
-ignoreNA <- T
-fonction_de_stat_agregee <- function(a, b = "moyenne", ...) {
-  ignoreNA <<- !ignoreNA
-  checkvalue <- F
-  for (x in c("moyenne", "variance", "ecart-type", "sd", "ecart type")) {
-    checkvalue <- (checkvalue | b == x)
-  }
-  if (checkvalue == FALSE) stop("statistique non supportée")
-
-  if (b == "moyenne") {
-    x <- mean(a, na.rm = ignoreNA, ...)
-  } else if (b == "ecart-type" | b == "sd" | b == "ecart type") {
-    x <- sd(b, na.rm = ignoreNA, ...)
-  } else if (a == "variance") {
-    x <- var(a, na.rm = ignoreNA, ...)
-  }
-  return(x)
-}
-fonction_de_stat_agregee(rnorm(10))
-fonction_de_stat_agregee(rnorm(10), "cart type")
-fonction_de_stat_agregee(rnorm(10), "ecart type")
-fonction_de_stat_agregee(rnorm(10), "variance")
 
 
-fonction_de_stat_agregee(df %>% filter(sexe == "Homme") %>% mutate(aged = as.numeric(aged)) %>% pull(aged), na.rm = T)
-fonction_de_stat_agregee(df2 %>% filter(sexe == "Femme") %>% mutate(aged = as.numeric(aged)) %>% pull(aged), na.rm = T)
-fonction_de_stat_agregee(df2 %>% filter(sexe == "Homme" & couple == "2") %>% mutate(aged = as.numeric(aged)) %>% pull(aged), na.rm = T)
-fonction_de_stat_agregee(df2 %>% filter(sexe == "Femme" & couple == "2") %>% mutate(aged = as.numeric(aged)) %>% pull(aged), na.rm = T)
+# MODELISATION ------------
 
-api_pwd <- "trotskitueleski$1917"
-
-# modelisation
-library(MASS)
 df3 <- df2 %>%
   select(surf, cs1, ur, couple, aged) %>%
   filter(surf != "Z")
